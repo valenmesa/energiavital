a2e1aec773be9f911e74d8494b8b13999e9b1d45
Matteus2021 <matteus2021estradauribe@gmail.com>
2023-08-18 07:24:32 -0500

    lo de alejo

--- proyectoEnergia/apps/servicios/views.py
+++ proyectoEnergia/apps/servicios/views.py
@@ -4,33 +4,49 @@ from apps.servicios.models import servicios
 from apps.servicios.form import serviciosForm
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
 # Create your views here.
 def index(request):
     return HttpResponse('Index servicio')
 
 
-class serviciosList(ListView):
+class serviciosList(LoginRequiredMixin, generic.ListView):
     model=servicios
     template_name='servicios/servicios_list.html'
+    context_object_name="obj"
+    login_url="login"
 
 #crear registro
-class serviciosCreate(CreateView):
+class serviciosCreate(LoginRequiredMixin, generic.CreateView):
     model=servicios
     form_class= serviciosForm
     template_name='servicios/servicios_form.html'
     success_url= reverse_lazy('servicios_listar')
+    context_object_name="obj"
+    login_url="login"
 
+    def form_valid(self, form):
+        form.instance.uc=self.request.user
+        return super().form_valid(form)
+    
 #Editar registro
-class serviciosEdit(UpdateView):
+class serviciosEdit(LoginRequiredMixin, generic.UpdateView):
     model=servicios
     form_class= serviciosForm
     template_name='servicios/servicios_form.html'
     success_url= reverse_lazy('servicios_listar')
+    context_object_name="obj"
+    login_url="login"
 
+    def form_valid(self, form):
+        form.instance.um=self.request.user.id
+        return super().form_valid(form)
+    
 # Eliminar registro
-class serviciosDelete(DeleteView):
+class serviciosDelete(LoginRequiredMixin, generic.DeleteView):
     model=servicios
     template_name='servicios/servicios_delete.html'
     success_url= reverse_lazy('servicios_listar')
+    context_object_name="obj"
